63fa4bbed572f175f8f41882786ed5b13b4d267e
Mithilesh Tiwari <Mithilesh@crowdbotics.com>
2022-11-09 00:47:40 +0530

    Crowdbotics Studio: Update app screens

---
+++ screens/Untitled1/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+const Untitled1 = () => {
+  return <View style={_styles.aaTAvwVm}>
+      </View>;
+};
+
+export default Untitled1;
+
+const _styles = StyleSheet.create({
+  aaTAvwVm: {
+    backgroundColor: "#f0f0f1",
+    padding: 10,
+    position: "relative",
+    height: "100%"
+  }
+});
