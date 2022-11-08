f63aefe3d7cd17fa3f08a5b10bc5717d66ad7b06
Crowdbotics <engineering@crowdbotics.com>
2022-11-08 17:37:02 +0000

    Initial commit.

---
+++ backend/vbdkdk_dev_43478/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for vbdkdk_dev_43478 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vbdkdk_dev_43478.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "vbdkdk_dev_43478",
+  url: "https://vbdkdk_dev_43478.botics.co",
+  api: "https://vbdkdk_dev_43478.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
